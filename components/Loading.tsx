445d44ddf08409809af4a8acb4cf3bb128b79033
Berl Treasure Campomanes <berlcamp@gmail.com>
2023-12-07 16:56:11 +0800

    first commit

---
+++ components/Loading.tsx
@@ -0,0 +1,18 @@
+export default function Loading () {
+  return (
+    <div className="animate-pulse mx-4 my-4">
+      <div className="grid grid-cols-1 gap-4 my-4">
+        <div className="h-4 w-24 bg-slate-200 dark:bg-slate-700 rounded col-span-1"></div>
+        <div className="h-4 bg-slate-200 dark:bg-slate-700 rounded col-span-1"></div>
+      </div>
+      <div className="grid grid-cols-1 gap-4 my-4">
+        <div className="h-4 w-24 bg-slate-200 dark:bg-slate-700 rounded col-span-1"></div>
+        <div className="h-4 bg-slate-200 dark:bg-slate-700 rounded col-span-1"></div>
+      </div>
+      <div className="grid grid-cols-1 gap-4 my-4">
+        <div className="h-4 w-24 bg-slate-200 dark:bg-slate-700 rounded col-span-1"></div>
+        <div className="h-4 bg-slate-200 dark:bg-slate-700 rounded col-span-1"></div>
+      </div>
+    </div>
+  )
+}
