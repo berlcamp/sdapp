39ab7091325ab3d692084a564b31ec24316fc7e0
Berl Treasure Campomanes <berlcamp@gmail.com>
2023-12-13 10:36:17 +0800

    >

--- components/NavLinks.tsx
+++ components/NavLinks.tsx
@@ -8,10 +8,10 @@ export const NavLinks = () => {
   return (
     <div className="space-x-4 space-y-2">
       {/* <Link className={`inline-flex ${currentRoute === '/' ? 'underline underline-offset-4 font-bold text-gray-200' : 'text-gray-300'}`} href='/'>Duplicates</Link>
-      <Link className={`inline-flex ${currentRoute === '/heads' ? 'underline underline-offset-4 font-bold text-gray-200' : 'text-gray-300'}`}  href='/heads'>Head&nbsp;SP</Link>
       <Link className={`inline-flex ${currentRoute === '/notregistered' ? 'underline underline-offset-4 font-bold text-gray-200' : 'text-gray-300'}`}  href='/notregistered'>Not&nbsp;Registered</Link>*/}
       <Link className={`inline-flex ${currentRoute === '/households' ? 'underline underline-offset-4 font-bold text-gray-200' : 'text-gray-300'}`}  href='/households'>Households</Link>
       <Link className={`inline-flex ${currentRoute === '/search' ? 'underline underline-offset-4 font-bold text-gray-200' : 'text-gray-300'}`}  href='/search'>Registered</Link>
+      <Link className={`inline-flex ${currentRoute === '/heads' ? 'underline underline-offset-4 font-bold text-gray-200' : 'text-gray-300'}`}  href='/heads'>Head&nbsp;SP</Link>
     </div>
   )
 }
