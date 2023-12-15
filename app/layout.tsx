860ebfe5c39cec58bad022bcbd81831fbee39433
Berl Treasure Campomanes <berlcamp@gmail.com>
2023-12-15 17:26:12 +0800

    ..

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,19 +24,19 @@ export default async function RootLayout({
   let barangays: string[] = []
 
   // Exclude non-whitelisted users
-  if (user && (id !== 'user_2ZA73AGLyPUkOXHfSWwNODxsMrG' && id !== 'user_2ZZIz46GmJaFLFpYIO9gNLWbjqu')) {
-    return (
-      <html lang="en" className='bg-black'>
-      <ClerkProvider>
-        <body className={inter.className}>
-          <div className='mt-4'>
-            <div className='text-center pt-10 text-bold text-gray-300'>You are not authorize to access this page.</div>
-          </div>
-        </body>
-      </ClerkProvider>
-    </html>
-    )
-  }
+  // if (user && (id !== 'user_2ZA73AGLyPUkOXHfSWwNODxsMrG' && id !== 'user_2ZZIz46GmJaFLFpYIO9gNLWbjqu')) {
+  //   return (
+  //     <html lang="en" className='bg-black'>
+  //     <ClerkProvider>
+  //       <body className={inter.className}>
+  //         <div className='mt-4'>
+  //           <div className='text-center pt-10 text-bold text-gray-300'>You are not authorize to access this page.</div>
+  //         </div>
+  //       </body>
+  //     </ClerkProvider>
+  //   </html>
+  //   )
+  // }
 
   return (
     <html lang="en" className='bg-black'>
